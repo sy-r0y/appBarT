1c20dded55f6c7b44b929bf0b6b823966cc3ab42
Soumya Roy <roy.valmont@gmail.com>
2012-09-01 00:23:15 +0530

    Route Info implemented

---
+++ as
@@ -0,0 +1,23 @@
+Millbrae|37.599787|#ff0000
+San Bruno|37.637753|#ff0000
+South San Francisco|37.664174|#ff0000
+Colma|37.684638|#ff0000
+Daly City|37.70612055|#ff0000
+Balboa Park|37.72198087|#ff0000
+Glen Park|37.732921|#ff0000
+24th St. Mission|37.752254|#ff0000
+16th St. Mission|37.765062|#ff0000
+Civic Center/UN Plaza|37.779528|#ff0000
+Powell St.|37.784991|#ff0000
+Montgomery St.|37.789256|#ff0000
+Embarcadero|37.792976|#ff0000
+West Oakland|37.80467476|#ff0000
+12th St. Oakland City Center|37.803664|#ff0000
+19th St. Oakland|37.80787|#ff0000
+MacArthur|37.828415|#ff0000
+Ashby|37.853024|#ff0000
+Downtown Berkeley|37.869867|#ff0000
+North Berkeley|37.87404|#ff0000
+El Cerrito Plaza|37.9030588|#ff0000
+El Cerrito del Norte|37.925655|#ff0000
+Richmond|37.936887|#ff00
--- getroute.php
+++ getroute.php
@@ -1,8 +1,53 @@
 <?php
 
 /* Get the route number from getRoute().
- * 
+ * Receive the "route" value via AJAX call from getroute() in index.html
+ * Sanitize it.
+ * Query bart.db
+ * Don't ship Junk!!
  */
 
+class RouteInfo {
+  public $stnAbbr;
+  public $stnName;
+  public $rtColor;
+  public $stnLan;
+  public $stnLong;
+  public $noerror=false;
+}
+//header("Content-type:application/json");
+$route=mysql_real_escape_string($_GET['route']);
+
+$db=new SQLite3('bart.db');
+
+//$route=mysql_real_escape_string($_POST['route']);
+//$result=$route."haha";
+
+/* SELECT stations.name,stations.abbr,stations.slan,stations.slong,routes.color,routes.name
+ * FROM stations,routes,rtstn
+ * WHERE stations.id=rtstn.rtid
+ * AND rtstn.rtid=routes.id
+ * AND rtstn.rtid='{$route}';
+ */
+/*$stmt=$db->prepare("SELECT stations.name,stations.abbr,stations.slan,stations.slong,routes.color,routes.name
+                    FROM stations,routes,rtstn
+                    WHERE stations.id=rtstn.stnid AND rtstn.rtid=routes.id AND rtstn.rtid='{$route}'");
+//$stmt->bindValue(':route',$route,SQLITE3_INTEGER);
+$result=$stmt->execute();
+*/
+
+$result=$db->query("SELECT stations.name,stations.abbr,stations.slan,stations.slong,routes.color,routes.name
+                    FROM stations,routes,rtstn
+                    WHERE stations.id=rtstn.stnid AND rtstn.rtid=routes.id AND rtstn.rtid='{$route}'");
+
+//$stmt->bindValue(':route',$route,SQLITE3_INTEGER);
+//$result=$stmt->execute();
+
+while($row=$result->fetchArray(SQLITE3_ASSOC)) {
+  echo "<pre>";
+  print(json_encode($row));
+  echo "</pre>";
+}
+//print(json_encode($result));
 
 ?>
