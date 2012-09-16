d4fa390d2265a3904412e184a3027a59f5096da6
Soumya Roy <roy.valmont@gmail.com>
2012-09-16 19:50:16 +0530

    V-0.2 Polylines Implemented

--- js/myjs.js
+++ js/myjs.js
@@ -1,13 +1,13 @@
 var map;
-var poly;
+var poly=[];
 var stations=[];
-//var route=[];
+var route=[];
 var Rt;
 var Clr;
-//var prepath=null;
-
+var polyroute;
 function init()
 {
+    alert("1. INSIDE init()");
     var elem=document.getElementById('canvas_map');
     var latlng=new google.maps.LatLng(37.752254,-122.418466);
     var opt={zoom:13,
@@ -23,56 +23,49 @@ function init()
 
 function draw(lat,lon,name) 
 {
-    /* Will draw the new station markers onto the map.
-     * Will create the info windows for the markers(stations).
-     * Will draw the polyline along the stations for showing the route map.
-     */ 
-    
-    var route=[];
+//    console.log("Lat:- "+lat+", Lon:- "+lon+", Name:- "+name+"Color:- "+Clr+" Route:- "+Rt);
     route.push(new google.maps.LatLng(lat,lon));
-    var mark=new google.maps.Marker({
-	position:route,
+
+  var mark=new google.maps.Marker({
+	position:new google.maps.LatLng(lat,lon),
 	title:name,
 	clickable:true,
 	map:map
     });
     stations.push(mark);
-    drawinfo(mark);
-    drawpoly(route);
+    drawinfo(mark,name);	  
 }
 
-function drawinfo(mark) {
-    var strContent='<div id="content" style="width:300px;height:100px;"><div id="stn" style="font-weight:bold;margin:auto;padding:2px;">Station: '+this.name+'</div><br/><div id="route" style="margin-top:0.5em;padding:2px;background-color:'+this.Clr+';float:left;font-size:0.8em;font-weight:bold">Route: '+this.Rt+'</div><div id="arrdep" style="margin-left:2px;float:right;"><table style="border:0px;"><thead>Arrival-Departure</thead><tr><td></td></tr></table></div></div>';
+function drawinfo(mark,name) {
+    var strContent='<div id="content" style="width:300px;height:100px;"><div id="stn" style="font-weight:bold;margin:auto;padding:2px;">Station: '+this.name+'</div><br/><div id="route" style="margin-top:0.5em;padding:2px;background-color:'+Clr+';float:left;font-size:0.8em;font-weight:bold">Route: '+Rt+'</div><div id="arrdep" style="margin-left:2px;float:right;"><table style="border:0px;"><thead>Arrival-Departure</thead><tr><td></td></tr></table></div></div>';
 
     var infowindow=new google.maps.InfoWindow({
 	content:strContent
     });
-    google.maps.event.addListener(this.mark,'click',function(){infowindow.open(map,this.mark);});
+    google.maps.event.addListener(mark,'click',function(){infowindow.open(map,mark);});
 }
 function drawpoly() {
-//    var route=[];
-    if(poly) {
-	poly.setPath(polyoptions);
-    }
-    else {
-	polyoptions={
+    var polyoptions={
 	    path:route,
 	    strokeColor:Clr,
 	    strokeWeight:10,
 	    strokeOpacity:1.0
 	};
-	poly=new google.maps.Polyline(polyoptions);
-
-	poly.setMap(map);
-//	return false;
-    }
+    polyroute=new google.maps.Polyline(polyoptions);
+    poly.push(polyroute);
+    polyroute.setMap(map);
 }
-
 function clearOverlays() {
     for(var i=0;i<stations.length;i++) {
 	stations[i].setMap(null); //Take the station markers one by one and clear them.
     }
     stations=[]; // De-reference all the station markers.
+    for(var j=0;j<poly.length;j++) {
+	poly[j].setMap(null);
+
+    }
+    poly=[]; //Dereference all the polyline elements.
+    route=[]; //Dereference all the station markers.
 }
 
 /* For getRoute():-
@@ -84,11 +77,11 @@ function clearOverlays() {
  * Change the center to the first station.
  */
 function getRoute(routenum) { 
+    alert("2. INSIDE getRoute()");
     clearOverlays();
-//    clearRoute();
     var stn;
     $(document).ready(function() {
-	var marker;var lat;var lon;var name;
+	var lat;var lon;var name;
 	if(routenum=='') { return false; }
 	$.getJSON('getroute.php',{route:routenum},function(data) {
             $.each(data,function(key,value) {
@@ -97,10 +90,8 @@ function getRoute(routenum) {
 		    $.each(this,function(k,v) {
 			if(k=='name'){Rt=v;}
 			if(k=='color'){Clr=v;}
-			//           stn=JSON.stringify({stn,key:{"name":name,"lat":lat,"lon":lon}});
 		    }); 
 		}
-		//          $.each(stn,function(x,y){$.each(this,function(p,q){console.log(p+" : "+q)});});
 		if(key=='station') { 
 		    alert("Key: Station");
 		    $.each(this,function(k,v) {
@@ -109,23 +100,17 @@ function getRoute(routenum) {
 			    if(k2=='name'){ name=v2; }
 			    if(k2=='slat'){ lat=v2; }
 			    if(k2=='slong'){ lon=v2; }
-//			    stn=JSON.stringify({stn,key:{"name":name,"lat":lat,"lon":lon}});
-			    
 			});
-//			var mark=new google.maps.Marker( {
-//			    position:new google.maps.LatLng(lat,lon),
-//			    title:name,clickable:true,map:map
-//			});
 			draw(lat,lon,name);
+			drawpoly();
+//			console.log("Lat:- "+lat+", Lon:- "+lon+", Name:- "+name);
 		    });
 		}
-		//draw(lat,lon,name);
+
             }); //$.each
 //            console.log("Route Name: "+Rt+" ,Route Color: "+Clr);
-//	    draw(lat,lon,name);
-	}); //$.getJSON
-//	draw(lat,lon,name);
-    });//$(document)
-   
 
+	}); //$.getJSON
+
+    });//$(document)
 }
