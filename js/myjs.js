ced8f0572058f9932c52126db622dd554c5c1a73
Soumya Roy <roy.valmont@gmail.com>
2012-09-29 00:05:27 +0530

    V1.0

--- js/myjs.js
+++ js/myjs.js
@@ -25,6 +25,9 @@ function init()
 function draw(lat,lon,abbr,name)
 {
 //    console.log("Lat:- "+lat+", Lon:- "+lon+", Name:- "+name+"Color:- "+Clr+" Route:- "+Rt);
+    var content='<div style="width:400px;"><div>Station: '+name+'</div>';
+    content+='<div style="margin-top:12%;float:left;background-color:'+Clr+'">'+Rt+'</div>';
+    content+='<div style="float:right;"><table style="border:0;font-weight:bold;"><tr><td>Departures<span style="font-size:0.6em;">(in Mins)</span></td></tr>';
     route.push(new google.maps.LatLng(lat,lon));
     var mark=new google.maps.Marker({
 	position:new google.maps.LatLng(lat,lon),
@@ -33,120 +36,32 @@ function draw(lat,lon,abbr,name)
 	map:map
     });
     stations.push(mark);
-    //    drawinfo(mark,abbr,name);	  
     google.maps.event.addListener(mark,'click',function() {
+	infowindow.setContent(null);
 	infowindow.close();
+	$.ajaxSetup({async:false}); /* To ensure, the DOM is not altered until data is at hand(otherwise                                             * setContent() gets called withouth any data at hand, which then overflows.
+				     */
 	$.getJSON("getetd.php",{abbr:abbr},function(result) {
 	    $.each(result,function(key,value) {
 		$.each(this,function(k,v) {
-		    //		    strContent+="<tr><td>|"+k+"</td>";
-		    //		strContent+='<tr><td style="font-size:0.5em;"></td>';
+//		    console.log('K:-'+k+', V:-'+v);
+		    content+='<tr style="font-size:0.5em;color:#666;"><td>'+k+':-</td>';
 		    $.each(this,function(k2,v2) {
 			$.each(this,function(k3,v3) {
-			    var content+=v3;
-			    console.log(k2+'  '+v3);
-//			    infowindow.setContent(content+"|"+v3);
-			    //			    strContent+="<td>"+v3+"</td>";
-			    //			strContent+='<td style="font-size:0.5em;"></td>';
+ 			    content+='<td>'+v3+'</td>';
+			    
 			});
 		    });
-		    //		    strContent+="</tr>";
+		    content+='</tr>';
 		});
 	    });
 	}); //getJSON() ends
-//	infowindow.setContent("HAHAHA:- "+mark.title);
+	content+='</table></div></div>';
+	infowindow.setContent(content);
 	infowindow.open(map,mark);
     }); //addListener() Ends.
     drawpoly();
 }
-//function drawinfo(mark,abbr,name) {
-//    var strContent='<div id="content" style="width:300px;height:100px;"><div id="stn" style="font-weight:bold;margin:auto;padding:2px;"><p>Station: '+name+'</p></div><br/><div id="route" style="margin-top:0.5em;padding:2px;background-color:'+Clr+';float:left;font-size:0.8em;font-weight:bold">HahaRoute: '+Rt+'</div><div id="arrdep" style="margin-left:2px;float:right;"><table style="border:0px;font-size:0.5em;color:#666"><thead>Departure</thead><tr><td>Destination: </td><td>Time1 </td><td>Time2 </td><td>Time3 </td></tr></table></div></div>';
-//    var strContent2;
-/*    google.maps.event.addListener(mark,'click',function(){
-	infowindow.close();
-	//	alert("KEY:- "+stn);
-	//	$.getJSON("getetd.php",{abbr:
-	infowindow.setContent(strContent);
-	$('#content').append("<p>Hello World</p>");
-	infowindow.setContent(strContent);
-	infowindow.open(map,mark);
-   });
-  */  
-/*    var strContent='<div id="content" style="position:absolute;overflow:auto;width:490px;height:200px;"><div id="stn" style="font-weight:bold;margin:auto;padding:0.5em;"><p>Station: '+name+'</p></div><br/><div id="route" style="margin-top:0.5em;padding:0.5em;background-color:'+Clr+';float:left;font-size:0.8em;font-weight:bold">Route: '+Rt+'</div><div id="arrdep" style="margin-left:0.5em;float:right;"><table style="border:0px;font-size:0.5em;color:#666"><thead><tr><td>Departures</td></tr></thead><tbody>';
-*/
-
-/*    var strContent="<div style=\"width:400px;height:240px;\"><div style=\"font-weight:bold;padding:0.5em;\"><p>Station:"+name+"</p></div><br/><div style=\"margin-top:0.3em;background-color:"+Clr+";float:left;font-size:0.8em;font-weight:bold;\"><p>Route:"+Rt+"</p></div><div style=\"float:right;\"><table style=\"border:0;color:#666;\"><tr><td>Departures</td></tr>";
-
-//  <tr><td>Destination: </td><td>Time1 </td><td>Time2 </td><td>Time3 </td></tr></table></div></div>;
-    $.getJSON("getetd.php",{abbr:abbr},function(result) {
-	$.each(result,function(key,value) {
-	    $.each(this,function(k,v) {
-		strContent+="<tr><td>|"+k+"</td>";
-//		strContent+='<tr><td style="font-size:0.5em;"></td>';
-		$.each(this,function(k2,v2) {
-		    $.each(this,function(k3,v3) {
-			strContent+="<td>"+v3+"</td>";
-//			strContent+='<td style="font-size:0.5em;"></td>';
-		    });
-		});
-		strContent+="</tr>";
-	    });
-	});
-    }); //getJSON() ends
-    strContent+="</table></div></div>";
-//    infowindow.setContent(strContent);
-    google.maps.event.addListener(mark,'click',function(){
-	infowindow.close();
-	infowindow.setContent(strContent);
-	infowindow.open(map,mark);
-    });
-    /*
-     {
-      "etd":{
-            "Dublin\/Pleasanton":[
-	              {"0":"Leaving"},
-		      {"0":"4"},
-		      {"0":"19"}
-		      ],
-	    "Fremont":[
-	              {"0":"11"},
-		      {"0":"26"},
-		      {"0":"41"}
-	              ],
-	    "Millbrae":[
-	               {"0":"3"},
-		       {"0":"28"},
-		       {"0":"34"}
-		       ],
-	    "Pittsburg\/Bay Point":[
-	               {"0":"7"},
-		       {"0":"22"},
-		       {"0":"37"}
-		       ],
-	    "Richmond":[
-	               {"0":"14"},
-		       {"0":"30"},
-		       {"0":"45"}
-		       ],
-	    "SF Airport":[
-	               {"0":"11"},
-		       {"0":"30"},
-		       {"0":"42"}
-		       ]
-	}
- }
- */
-/*    var strContent='<div id="content" style="width:300px;height:100px;"><div id="stn" style="font-weight:bold;margin:auto;padding:2px;"><p>Station: '+name+'</p></div><br/><div id="route" style="margin-top:0.5em;padding:2px;background-color:'+Clr+';float:left;font-size:0.8em;font-weight:bold">Route: '+Rt+'</div><div id="arrdep" style="margin-left:2px;float:right;"><table style="border:0px;"><thead>Departure</thead><tr><td></td></tr></table></div></div>';
-
-    google.maps.event.addListener(mark,'click',function(){
-	infowindow.close();
-//	alert("KEY:- "+stn);
-//	$.getJSON("getetd.php",{abbr:
-	infowindow.setContent(strContent);
-	infowindow.open(map,mark);
-    });*/
-//}
-
 function drawpoly() {
     var polyoptions={
 	    path:route,
